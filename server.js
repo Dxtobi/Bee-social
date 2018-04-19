d8b04536c1277208d30e458a44f8403490d726a5
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-04-19 09:32:37 -0700

    MongoDb config, connecting with Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//db config
+const db = require('./config/keys').mongoURI;
+
+//db connection
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDb connected'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
 

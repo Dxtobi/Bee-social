966b7a5d9c638183f2c9633c47e891e721e14a80
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-04-20 15:48:43 -0700

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
-router.get('/test', (req, res) => {
-    res.json({
-        message: 'Users works!'
-    })
+const User = require('../../models/User');
+
+router.get('/test', (req, res) =>  res.json({ message: 'Users works!'}));
+
+router.post('/register', (req, res) => {
+    User.findOne({ email : req.body.email })
+        .then(user => {
+        if(user){
+            return res.status(400).json({ email: 'email already exists'});
+        }else{
+            const avatar = gravatar.url(req.body.email,{
+                s: '200',//size    
+                r: 'pg',//rating    
+                d: 'mm',//default
+            });
+            
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password,
+            });
+            
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                })
+            })
+        }
+    });
 });
 
 module.exports = router;

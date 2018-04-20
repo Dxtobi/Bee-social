c0aaf5837c1b974ef9f11280cf3fd5b05f5ce25e
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-04-20 14:45:16 -0700

    Routes module config

---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+    res.json({
+        message: 'Profile works!'
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 //db config
@@ -14,6 +18,14 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello'));
 
+//middleware
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
+

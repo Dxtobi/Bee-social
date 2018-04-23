c4508c8f7d15de972432a038059f6a2e5b825641
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-04-22 20:55:55 -0700

    Login module implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -39,4 +39,27 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    User.findOne({email})
+        .then( user => {
+        //check for user
+        if(!user){
+            return res.status(404).json({ email: 'User not found!' });
+        }
+        
+        //check pass
+        bcrypt.compare(password, user.password)
+            .then( isMatch => {
+            if(isMatch){
+                res.json({msg: 'Success'});
+            }else{
+                return res.status('400').json({password: 'Password incorrect!'});
+            }
+        });
+    });
+});
+
 module.exports = router;

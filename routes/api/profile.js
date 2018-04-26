b0d6e81d43c2e62f0721518464e9dde74d6fde2a
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-04-26 13:58:35 -0700

    Profile model and first protectedt route implementation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,26 @@
-const express = require('express');
+const express = require( 'express' );
 const router = express.Router();
+const mongoose = require( 'mongoose' );
+const passport = require( 'passport' );
 
-router.get('/test', (req, res) => {
-    res.json({
-        message: 'Profile works!'
-    })
+const Profile = require( '../../models/Profile' );
+const User = require( '../../models/User' );
+
+router.get( '/test', ( req, res ) => { res.json({ message: 'Profile works!' }) });
+
+router.get( '/', passport.authenticate( 'jwt', { session: false } ), ( req, res ) => {
+    
+    const errors = { };
+    
+    Profile.findOne( { user: req.user.id } )
+        .then( profile => {
+            if( !profile ){
+                errors.noprofile = 'There is no profile for this user';
+                return res.status( 400 ).json( errors );
+            }   
+            res.json( profile );
+        })
+        .catch( err => res.status( 404 ).json( err ) );
 });
 
 module.exports = router;

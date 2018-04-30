1044aae4877ee6a64f46ba07916118ab9cc97006
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-04-30 14:18:48 -0700

    Components implementation

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default () => {
+    return (
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+            Copyright &copy; { new Date().getFullYear() } Developers Connector
+        </footer>
+    )
+}

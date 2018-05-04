880fa6bd8679c34a7c94815a27655d668b4c18aa
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-05-04 12:11:23 -0700

    Logout functionality implementation

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -45,3 +45,12 @@ export const setCurrentUser = decoded => {
     payload: decoded
   }
 }
+
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem( 'jwtToken' );
+  // Remove auth header for future requests
+  setAuthToken( false );
+  // Set the current user to {} which will set isAuthenticated to false
+  dispatch( setCurrentUser( { } ) );
+}
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -22,8 +22,14 @@ class Login extends Component {
     this.setState( { [e.target.name]: e.target.value } );
   }
 
+  componentDidMount(){
+    if( this.props.auth.isAuthenticated ){
+      this.props.history.push( '/dashboard' );
+    }
+  }
+
   componentWillReceiveProps( nextProps ){
-    
+
     if( nextProps.auth.isAuthenticated ){
         this.props.history.push( '/dashboard' );
     }

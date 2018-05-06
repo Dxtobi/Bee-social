79dc060b1298528d25209a55e6400abf19142131
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-05-06 13:52:52 -0700

    Private route component implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -13,6 +13,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/common/PrivateRoute';
 
 import './App.css';
 
@@ -50,7 +51,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
-              <Route exact path="/dashboard" component={ Dashboard } />
+              <Switch>
+               <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch> 
             </div>
           <Footer/>
         </div>

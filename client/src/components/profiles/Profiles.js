8acf34894f8a663aff7e60c99c650ef980a6d943
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-05-17 14:44:40 -0700

    Profile item implementation

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../common/Spinner';
 import { getProfiles } from '../../actions/profileActions';
+import ProfileItem from './ProfileItem';
 
 
 class Profiles extends Component {
@@ -21,7 +22,9 @@ class Profiles extends Component {
       profileItems = <Spinner/>;
     }else{
       if(profiles.length > 0){
-        profileItems = <h1>profile here</h1>;
+        profileItems = profiles.map( profile => (
+          <ProfileItem key={profile._id} profile={profile}/>
+        ))
       }else{
         profileItems = <h4>No profiles found...</h4>;
       }

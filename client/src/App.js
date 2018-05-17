433b4a13ce4fac79fb3080026538a5fc8f8c77bf
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-05-17 15:15:18 -0700

    Profile by handle and components implementation

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddEducation from './components/add-credentials/AddEducation';
 import AddExperience from './components/add-credentials/AddExperience';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 import './App.css';
 
@@ -57,6 +58,7 @@ class App extends Component {
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
               <Route exact path="/profiles" component={ Profiles }/>
+              <Route exact path="/profile/:handle" component={ Profile }/>
               <Switch>
                <PrivateRoute exact path="/dashboard" component={ Dashboard } />
                <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProfileAbout extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>TODO:PROFILE ABOUT</h1>
+            </div>
+        )
+    }
+}
+
+export default ProfileAbout;

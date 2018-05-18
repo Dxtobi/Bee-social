70fd05029514bb7144218f23b209508b0abfae16
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-05-17 18:48:55 -0700

    Profile header implementation

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,12 +18,38 @@ class Profile extends Component {
         }
     }
     render() {
-        return (
+
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if(profile === null || loading ){
+        profileContent = <Spinner/>
+    } else {
+        profileContent = (
             <div>
-                <ProfileHeader/>
+                <div className="row">
+                <div className="col-md-6">
+                    <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                        Back to Profiles
+                    </Link>
+                </div>
+                <div className="col-md-6"/>
+                </div>
+                <ProfileHeader profile={profile}/>
                 <ProfileAbout/>
                 <ProfileCreds/>
                 <ProfileGithub/>
+            </div>
+        )
+    }
+
+        return (
+            <div className="profile">
+                <div className="container">
+                    <div className="row">
+                    <div className="col-md-12">{profileContent}</div>
+                    </div>
+                </div>
             </div>
         )
     }

2d08a83463b79101ff31452419f101e124df176e
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-05-18 13:25:28 -0700

    Profile about component implementation

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -36,7 +36,7 @@ class Profile extends Component {
                 <div className="col-md-6"/>
                 </div>
                 <ProfileHeader profile={profile}/>
-                <ProfileAbout/>
+                <ProfileAbout profile={profile}/>
                 <ProfileCreds/>
                 <ProfileGithub/>
             </div>
